0cfde83414cf3276bc9689716819f82a9ea81a54
Riboprotein <riboprotein@gmail.com>
2024-12-03 19:58:40 -0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/satisfactory-module-mapper/",
   build: {
     outDir: 'docs'  // Changed from 'dist' to 'docs'
   },
